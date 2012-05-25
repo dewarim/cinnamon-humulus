1c6d18cbc97b70e840ead216a1fd9b0500791293
Ingo Wiarda <ingo_wiarda@dewarim.de>
2012-05-25 12:25:36 +0200

    initial import

---
+++ grails-app/conf/BuildConfig.groovy
@@ -0,0 +1,43 @@
+grails.project.class.dir = "target/classes"
+grails.project.test.class.dir = "target/test-classes"
+grails.project.test.reports.dir = "target/test-reports"
+grails.project.target.level = 1.6
+grails.project.source.level = 1.6
+//grails.project.war.file = "target/${appName}-${appVersion}.war"
+grails.project.repos.default = "myRepo"
+
+grails.project.dependency.resolution = {
+    // inherit Grails' default dependencies
+    inherits("global") {
+        // uncomment to disable ehcache
+        // excludes 'ehcache'
+    }
+    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    repositories {
+        mavenLocal()
+        mavenRepo name:'myRepo'
+        grailsRepo "http://grails.org/plugins"
+        grailsCentral()
+        // uncomment the below to enable remote dependency resolution
+        // from public Maven repositories
+        mavenCentral()
+        //mavenLocal()
+        //mavenRepo "http://snapshots.repository.codehaus.org"
+        //mavenRepo "http://repository.codehaus.org"
+        //mavenRepo "http://download.java.net/maven/2/"
+        //mavenRepo "http://repository.jboss.com/maven2/"
+    }
+    dependencies {
+        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
+        runtime 'org.mindrot:jbcrypt:0.3m'
+        // runtime 'mysql:mysql-connector-java:5.1.5'
+    }
+
+    plugins {
+        compile(':spring-security-core:1.2.7.2')
+        build(":tomcat:$grailsVersion",
+              ":release:2.0.2") {
+            export = false
+        }
+    }
+}
---
+++ src/groovy/humulus/HashMaker.groovy
@@ -0,0 +1,47 @@
+package humulus;
+
+import org.mindrot.jbcrypt.BCrypt
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+/**
+ * Create hash digests of a string, salted with a salt value from the config.  
+ *
+ */
+public class HashMaker {
+
+    static Integer passwordRounds = 12;
+    
+	/**
+	 * Use Bcrypt to create a hashed string from a given text.
+	 * Use before storing a password in the database.<br>
+	 * Used by server.UserAccount.
+     * @param text the password string from which a digest will be created.
+     * @return a digest based upon the configured number of hash generation rounds and the specified plain-text.
+	 */
+	public static String createDigest(String text){
+		Logger log = LoggerFactory.getLogger(HashMaker.class);
+		Integer rounds = passwordRounds
+		String digest = BCrypt.hashpw(text, BCrypt.gensalt(rounds));
+		log.debug("digest:"+digest);
+		return digest;
+	}
+
+    /**
+     * Compare the hashed version of a given text with a specified hash string.
+     * This method is used by the class SecurityController in Dandelion.
+     * @param text the plain text password
+     * @param hash the password hash
+     * @return true if the hash is neither null nor different from the hashed text.
+     */
+	public static Boolean compareWithHash(String text, String hash){
+        try{
+            return (hash != null) && ( BCrypt.checkpw(text, hash) );
+        }
+        catch (Exception e){
+            Logger log = LoggerFactory.getLogger(HashMaker.class);
+            log.warn("An exception occurred - failed to check password. Access denied!",e);
+            return false;
+        }
+	}
+}
